4405afd7200813b6c207a5e6a9e7d70bf64de25c
Monatric <nawpnawp8@gmail.com>
2025-07-13 22:43:03 +0800

    feat: add validations for event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :location, presence: true, length: { maximum: 50 }
   validates :event_date, presence: true
 
   belongs_to :creator, class_name: "User"

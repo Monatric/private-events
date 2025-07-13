268f8157171524cc94579c9897d22924e19c1c98
Monatric <nawpnawp8@gmail.com>
2025-07-13 15:46:02 +0800

    feat: permit username and add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :username, presence: true, length: { in: 3..20 }, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { in: 6..20 }
+
   has_many :events, foreign_key: :creator_id
 end

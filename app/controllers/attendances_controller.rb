682b26796213f0b984413ea953176af8bb5f664d
Monatric <nawpnawp8@gmail.com>
2025-07-15 03:51:21 +0800

    help: not sure how to make join work

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,4 +1,8 @@
 class AttendancesController < ApplicationController
+  before_action :authenticate_user!, only: [ :join ]
+
   def join
+    @attended_event = current_user.attended_events.build
+    puts params
   end
 end

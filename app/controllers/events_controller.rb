4d8554a9070dd3fb4da2e51f32b13804cd26b96c
Monatric <nawpnawp8@gmail.com>
2025-07-14 16:08:16 +0800

    feat: add actions for event creation in ctrl

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,25 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = current_user.events.build
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+
+    if @event.save
+      redirect_to root_url, notice: "Event created successfully!"
+    else
+      flash.now[:alert] = "Event cannot be created."
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def event_params
+      params.expect(event: [ :title, :location, :details, :event_date ])
+    end
 end

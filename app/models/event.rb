c91e674174b4b39a34806717025c2eddb010ffaa
Monatric <nawpnawp8@gmail.com>
2025-07-14 19:18:03 +0800

    feat: validate event date submission

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
   validates :title, presence: true, length: { maximum: 50 }
   validates :location, presence: true, length: { maximum: 50 }
-  validates :event_date, presence: true
+  validates :event_date, presence: true, comparison: { greater_than: 1.day.from_now, less_than: 1.year.from_now }
 
   belongs_to :creator, class_name: "User"
 end

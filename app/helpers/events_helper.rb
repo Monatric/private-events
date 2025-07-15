202b6092b8b507fdccd96a028ade6b023b638d94
Monatric <nawpnawp8@gmail.com>
2025-07-15 20:09:34 +0800

    feat: add helpers for event joining

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,9 @@
 module EventsHelper
+  def event_joined?(user, event_id)
+    user.attended_event_ids.include?(event_id)
+  end
+
+  def event_creator?(user, event_creator_id)
+    user.id == event_creator_id
+  end
 end

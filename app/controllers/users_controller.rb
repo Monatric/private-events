c07b467b108bd33a9d24a984fbe1ad786ba61cca
Monatric <nawpnawp8@gmail.com>
2025-07-15 23:55:55 +0800

    refactor: separate conditional viewing of show action in users ctrl

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,15 @@
 class UsersController < ApplicationController
+  before_action :require_same_account
+
   def show
     @user = current_user
-
-    if current_user.username != params[:username]
-      redirect_to root_url, notice: "You are not allowed to view that."
-    end
   end
+
+  private
+
+    def require_same_account
+      if !user_signed_in? || current_user.username != params[:username]
+        redirect_back fallback_location: root_url, alert: "You are not allowed to view that."
+      end
+    end
 end

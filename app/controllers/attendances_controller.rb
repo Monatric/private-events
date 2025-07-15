bce4f57a574b8dc1fe1aa7c5f04acd76ddbadff4
Monatric <nawpnawp8@gmail.com>
2025-07-15 20:10:03 +0800

    feat: add logic for joining events in ctrl

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -2,7 +2,13 @@ class AttendancesController < ApplicationController
   before_action :authenticate_user!, only: [ :join ]
 
   def join
-    @attended_event = current_user.attended_events.build
-    puts params
+    @event = Event.find(params[:event_id])
+
+    if current_user.attended_events << @event
+      redirect_back fallback_location: root_url, notice: "Joined successfully!"
+    else
+      flash.now[:alert] = "Something went wrong."
+      render "events/show", status: :unprocessable_entity
+    end
   end
 end

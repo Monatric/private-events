9fd688db00994ce8cebf8fbaab188e7687aa114a
Monatric <nawpnawp8@gmail.com>
2025-07-16 17:38:05 +0800

    refactor: use #create! in attendance

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -4,11 +4,8 @@ class AttendancesController < ApplicationController
   def create
     @event = Event.find(params[:event_id])
 
-    if current_user.attendances.create(attended_event: @event)
+    if current_user.attendances.create!(attended_event: @event)
       redirect_back fallback_location: root_url, notice: "Joined successfully!"
-    else
-      flash.now[:alert] = "Something went wrong."
-      render "events/show", status: :unprocessable_entity
     end
   end
 end

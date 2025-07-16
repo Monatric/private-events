a1fd4c8177a7eaefd0257a19d9b8d152c0cb6be1
Monatric <nawpnawp8@gmail.com>
2025-07-16 18:19:44 +0800

    feat: add delete btn and logic for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -40,6 +40,12 @@ class EventsController < ApplicationController
     end
   end
 
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to root_url, notice: "Event deleted sucessfully!"
+  end
+
   private
 
     def event_params

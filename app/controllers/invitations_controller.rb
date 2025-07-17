3955ce38df2497c937c9a213f947c76ca4f2e8bd
Monatric <nawpnawp8@gmail.com>
2025-07-17 20:17:20 +0800

    feat: add before action for invitation

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,4 +1,7 @@
 class InvitationsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :verify_invitation_existence, only: [ :create ]
+
   def create
     @event = Event.find(params[:event])
     @invitee = User.find(params[:invitee])
@@ -6,4 +9,16 @@ class InvitationsController < ApplicationController
     current_user.invitations.create!(event: @event, invitee: @invitee)
     redirect_back fallback_location: root_url, notice: "Invited #{@invitee.username} successfully!"
   end
+
+  private
+
+    def verify_invitation_existence
+      @event = Event.find(params[:event])
+      @invitee = User.find(params[:invitee])
+
+      if current_user.invitations.where(invitee_id: @invitee, event_id: @event).any?
+        # if Invitation.invited_certain_user?(current_user, @invitee, @event).any?
+        redirect_back fallback_location: root_url, alert: "#{@invitee.username} has already been invited."
+      end
+    end
 end

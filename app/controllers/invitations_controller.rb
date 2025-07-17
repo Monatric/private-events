571561e7746b9d0d89581cb3a1f01e7f95113a59
Monatric <nawpnawp8@gmail.com>
2025-07-17 23:49:26 +0800

    refactor: use invitees for inviting in create actoin

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -6,7 +6,7 @@ class InvitationsController < ApplicationController
     @event = Event.find(params[:event])
     @invitee = User.find(params[:invitee])
 
-    current_user.invitations.create!(event: @event, invitee: @invitee)
+    current_user.invitees.create!(event: @event, invitee: @invitee)
     redirect_back fallback_location: root_url, notice: "Invited #{@invitee.username} successfully!"
   end
 
